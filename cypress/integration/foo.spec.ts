7a05fa005abdc8c5bd25b4eae201cc025b73f613
vadym-tarasich <91530582+vadym-tarasich@users.noreply.github.com>
2022-02-18 09:58:35 +0200

    Initial commit

---
+++ cypress/integration/foo.spec.ts
@@ -0,0 +1,12 @@
+// import function from the application source
+import { sum } from '../../src/foo'
+
+describe('TypeScript spec', () => {
+  it('works', () => {
+    cy.wrap('foo').should('equal', 'foo')
+  })
+
+  it('calls TS source file', () => {
+    expect(sum(1, 2, 3, 4)).to.equal(10)
+  })
+})
